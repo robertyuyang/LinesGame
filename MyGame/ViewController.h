2275b9f562e579761aceead22d41a5eb05a7c96e
Xie He <kobedou@gmail.com>
2016-09-05 15:34:59 +0800

    Initial Commit

---
+++ MyGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGame
+//
+//  Created by harmonyshoes on 16/9/5.
+//  Copyright © 2016年 robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyGame
+//
+//  Created by harmonyshoes on 16/9/5.
+//  Copyright © 2016年 robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
